3ea678f2db6ccbd4808029d6f5fe66fc4d994e98
feherszabolcs <feherszabi2005@gmail.com>
2024-11-05 22:48:18 +0100

    :sparkles: mx recording

--- components/creategrid/create.c
+++ components/creategrid/create.c
@@ -14,12 +14,29 @@ static void Print()
     printf("\n********************************\n\n");
 }
 
+// matrix allapot kirajzolo fgv
+void MxTest(char **mx, int size)
+{
+    for (int i = 0; i <= size + 1; i++)
+    {
+        for (int j = 0; j <= size + 1; j++)
+        {
+            if (i == 0 || j == 0 || i == size + 1 || j == size + 1)
+                printf("*");
+            else
+                printf("%c", mx[i - 1][j - 1]);
+        }
+        printf("\n");
+    }
+}
+
 void Creator()
 {
 
     Print();
     int mxSize;
     char input;
+    char **mx;
 
     while (scanf("%c", &input) == 1)
     {
@@ -31,8 +48,32 @@ void Creator()
                 while (getchar() != '\n')
                     ;
             }
-            printf("\n* A valasztott meret: %d x %d *", mxSize, mxSize);
+            printf("\n* A valasztott meret: %d x %d *\n", mxSize, mxSize);
+
+            // jatekter lefoglalasa a memoriaban
+            mx = (char **)calloc(mxSize, sizeof(char *));
+            for (int i = 0; i < mxSize; i++)
+                mx[i] = (char *)calloc(mxSize, sizeof(char));
+            sleep_ms(1000);
+
+            for (int i = 1; i <= mxSize; i++)
+            {
+                char *line = (char *)calloc(mxSize + 1, sizeof(char));
+                if (line == NULL)
+                    printf("Nem sikerult lefoglalni a memoriat a jatekternek!\n");
+                printf("Adja meg a %d. sorban az eletallapotokat (pl.: __X_ , ahol a _ karakter szokozt jelol) es %d oszlopbol alljon!: ", i, mxSize);
+                scanf("%s", line);
+                // printf("%s", line);
+                for (int j = 0; j < mxSize; j++)
+                {
+                    mx[i - 1][j] = line[j];
+                }
+            }
+
+            MxTest(mx, mxSize);
             break;
+            // jatekter feltoltese TODO
+
             // jatek kezdete ----
         }
         if (input == 'b')
