1717ff319c09ba06a774faa07154b70a74648b86
feherszabolcs <feherszabi2005@gmail.com>
2024-11-10 14:49:09 +0100

    :sparkles: Added random mx generation

--- components/creategrid/create.c
+++ components/creategrid/create.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../menu/menu.h"
+#include <time.h>
 
 int mxSizeX;
 int mxSizeY;
@@ -13,7 +14,8 @@ static void Print()
     system("cls");
     printf("\n********************************");
     printf("\n*     Jatekter letrehozas      *");
-    printf("\n*     1 - Tovabb               *");
+    printf("\n*     1 - Manualis bevitel     *");
+    printf("\n*     2 - Veletlenszeru adatok *");
     printf("\n*     b - Vissza a menube      *");
     printf("\n********************************\n\n");
 }
@@ -34,49 +36,68 @@ void MxTest(char **mx, int sizeX, int sizeY)
     }
 }
 
+void FillMatrix()
+{
+    for (int i = 1; i <= mxSizeX; i++)
+    {
+        char *line = (char *)calloc(mxSizeY + 2, sizeof(char));
+        if (line == NULL)
+            printf("Nem sikerult lefoglalni a memoriat a jatekternek!\n");
+        printf("Adja meg a %d. sorban az eletallapotokat (pl.: __X_ , ahol a _ karakter szokozt jelol) es %d oszlopbol alljon!: ", i, mxSizeY);
+        scanf("%s", line);
+
+        // VALOS INPUT ELLENORZES (tenyleg x és _)
+
+        // printf("%s", line);
+        for (int j = 1; j <= mxSizeX + 1; j++)
+        {
+            mx[i][j] = line[j - 1];
+        }
+    }
+}
+
 void Creator()
 {
+    srand(time(NULL));
     Print();
     char input;
 
     while (scanf("%c", &input) == 1)
     {
-        if (input == '1')
+        if (input == '1' || input == '2')
         {
-            printf("Adja meg a jatekter meretet (pl.: 5x5 eseten): ");
+            printf("Adja meg a jatekter meretet (pl.: 5x4): ");
             while (scanf("%dx%d", &mxSizeX, &mxSizeY) != 2)
             {
                 while (getchar() != '\n')
                     ;
             }
             printf("\n* A valasztott meret: %d x %d *\n", mxSizeX, mxSizeY);
-
             // jatekter lefoglalasa a memoriaban
             mx = (char **)calloc(mxSizeX + 2, sizeof(char *));
             for (int i = 0; i < mxSizeX + 2; i++)
                 mx[i] = (char *)calloc(mxSizeY + 2, sizeof(char));
             sleep_ms(1000);
-
+        }
+        // user altal bevitt adatbol generalas
+        if (input == '1')
+        {
+            FillMatrix();
+            break;
+        }
+        // jatekter random generalasa
+        if (input == '2')
+        {
             for (int i = 1; i <= mxSizeX; i++)
             {
-                char *line = (char *)calloc(mxSizeY + 2, sizeof(char));
-                if (line == NULL)
-                    printf("Nem sikerult lefoglalni a memoriat a jatekternek!\n");
-                printf("Adja meg a %d. sorban az eletallapotokat (pl.: __X_ , ahol a _ karakter szokozt jelol) es %d oszlopbol alljon!: ", i, mxSizeY);
-                scanf("%s", line);
-
-                // VALOS INPUT ELLENORZES (tenyleg x és _)
-
-                // printf("%s", line);
-                for (int j = 1; j <= mxSizeX + 1; j++)
+                for (int j = 1; j <= mxSizeY; j++)
                 {
-                    mx[i][j] = line[j - 1];
+                    int r = rand() % 2;
+                    mx[i][j] = r == 0 ? '_' : 'X';
                 }
             }
 
             break;
-
-            // jatek kezdete ----
         }
         if (input == 'b')
         {
